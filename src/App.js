bbebe4e4fc16072b998cb7d9c33616cfa304aa7b
Chris Chapman <chrischapman900@gmail.com>
2020-05-07 01:48:46 -0400

    add error boundary

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
+import {Cell, Grid} from "react-foundation";
+
 import './App.css';
+
+import {WeatherContextProvider} from "./contexts/weather";
+
+import ErrorBoundary from './ErrorBoundary'
 import Weather from "./components/Weather/Weather";
 import Clock from "./components/Clock/Clock";
-import {Cell, Grid} from "react-foundation";
 import Zmanim from "./components/Events/Zmanim";
-import {WeatherContextProvider} from "./contexts/weather";
 
 function App() {
   return (
-    <div className="App">
-      <WeatherContextProvider>
-      <Grid style={{borderBottom: 'solid 1px'}}>
-        <Cell small={8}>
-          <Clock/>
-        </Cell>
-        <Cell small={4}>
-          <Zmanim/>
-        </Cell>
-      </Grid>
-      <Weather/>
-      </WeatherContextProvider>
-    </div>
+    <ErrorBoundary>
+      <div className="App">
+        <WeatherContextProvider>
+        <Grid style={{borderBottom: 'solid 1px'}}>
+          <Cell small={8}>
+            <Clock/>
+          </Cell>
+          <Cell small={4}>
+            <Zmanim/>
+          </Cell>
+        </Grid>
+        <Weather/>
+        </WeatherContextProvider>
+      </div>
+    </ErrorBoundary>
   );
 }
 
